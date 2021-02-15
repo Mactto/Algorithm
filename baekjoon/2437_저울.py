0a4c48aef44c6bbe841cffa174a343f74f9b7e58
Mactto <shc9928@gmail.com>
2021-02-15 11:20:13 +0900

    2437

---
+++ baekjoon/2437_저울.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+# 처음 푼 풀이
+'''
+def algorithm():
+    from itertools import combinations
+    enable = set(weights)
+    for i in range(2, N+1):
+        combi = list(combinations(weights, i))
+        for c in combi:
+            enable.add(sum(c))
+    
+    for i in range(1, max(enable)):
+        if i not in enable:
+            return i
+'''        
+
+# 수정 풀이
+def algorithm():
+    result = 1
+    weights.sort()
+
+    for i in weights:
+        if result<i:
+            print(result)
+        result+=i
+    
+
+if __name__ == "__main__":
+    N = int(input())
+    weights = list(map(int, input().split()))
+    print(algorithm())
