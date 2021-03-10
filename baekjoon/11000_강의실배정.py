c3b3b4db1d3ca97df468e5bb9a9a723eff903e67
Mactto <shc9928@gmail.com>
2021-03-10 10:56:37 +0900

    11000

---
+++ baekjoon/11000_강의실배정.py
@@ -0,0 +1,20 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+def algorithm():
+    for i in range(N):
+        if len(arr) != 0 and arr[0] <= time[i][0]:
+            heapq.heappop(arr)
+        heapq.heappush(arr, time[i][1])
+
+    return len(arr)
+
+    
+
+if __name__ == "__main__":
+    N = int(input())
+    time = [list(map(int, input().split())) for _ in range(N)]
+    arr = []
+    time.sort(key=lambda x: x[0])
+    print(algorithm())
