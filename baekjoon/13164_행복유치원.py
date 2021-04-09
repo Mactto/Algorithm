f9bf4233fa02bde098b342a47d236f9cc02ec0ab
Mactto <shc9928@gmail.com>
2021-04-09 11:54:58 +0900

    13164

--- baekjoon/13164_행복유치원.py
+++ baekjoon/13164_행복유치원.py
@@ -4,19 +4,17 @@ input = sys.stdin.readline
 def algorithm():
     diff = []
 
-    for i in range(N-1):
-        diff.append(height[i+1] - height[i])
+    for i in range(1, N):
+        diff.append(height[i] - height[i-1])
     diff.sort()
 
     answer = 0
     for i in range(N-K):
+        print(diff[i])
         answer += diff[i]
     return answer
         
-
-        
-
 if __name__ == "__main__":
     N, K = map(int, input().split())
-    height = [list(map(int, input().split()))]
+    height = list(map(int, input().split()))
     print(algorithm())
