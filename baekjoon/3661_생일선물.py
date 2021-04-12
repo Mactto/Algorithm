c40ddc71fb6540fcf31e9d5ce850e5e78b5aa862
Mactto <shc9928@gmail.com>
2021-04-13 02:57:38 +0900

    3661

--- baekjoon/3661_생일선물.py
+++ baekjoon/3661_생일선물.py
@@ -36,32 +36,3 @@ if __name__ == "__main__":
         enable = list(map(int, input().split()))    # 낼 수 있는 금액
         enable.sort()
         print(algorithm())
-
-'''
-20 4
-10 10 4 4 
-
-remain = 12
-12 / 2 = 6
-6 / 1 = 6
-
-
-7 3
-1 1 4
-
-remain = 5
-5 / 1 = 5
-
-
-
-34 5
-9 8 8 8 4
-
-remain = 30
-
-30 / 4 = 7
-23 / 3 = 7
-16 / 2 = 8
-8 / 1 = 8
-
-'''
