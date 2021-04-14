58d012801ecdae8343039c21e7a746b238bf5159
Mactto <shc9928@gmail.com>
2021-04-14 15:19:09 +0900

    1781

--- baekjoon/1781_컵라면.py
+++ baekjoon/1781_컵라면.py
@@ -9,15 +9,12 @@ def algorithm(info):
 
     for i in info:
         heapq.heappush(heap, i[1])
-        print("heap push : ", heap)
         if i[0] < len(heap):
             heapq.heappop(heap)
-            print("heap pop : ", heap)
     return sum(heap)
 
 if __name__ == "__main__":
     N = int(input())
     info = [list(map(int, input().split())) for _ in range(N)]
     info.sort()
-    print(info)
     print(algorithm(info))
