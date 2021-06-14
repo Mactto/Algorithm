cc674027c8ff2ae5560c8d9f34d4a2ab69a0c89a
Mactto <shc9928@gmail.com>
2021-06-14 11:14:50 +0900

    2138

--- baekjoon/2138_전구와스위치.py
+++ baekjoon/2138_전구와스위치.py
@@ -1,13 +1,50 @@
 import sys
 input = sys.stdin.readline
 
-def algorithm(state, count):
-    queue = [state]
+def switch(idx, state):
+    for i in range(idx-1, idx+2):
+        if -1 < i and i < N:
+            if state[i] == '0':
+                state[i] = '1'
+            else:
+                state[i] = '0'
+    return state
 
-    while queue:
-        
+def zeroAlgorithm(state):
+    count = 0
+    for idx in range(1, N):
+        if state[idx-1] != answer[idx-1]:
+            state = switch(idx, state)
+            count += 1
+    if ''.join(state) == answer:
+        return count
+    else:
+        return -1
+
+def zeroSwitchAlgorithm(state):
+    count = 1
+    state = switch(0, state)
+    for idx in range(1, N):
+        if state[idx-1] != answer[idx-1]:
+            state = switch(idx, state)
+            count += 1
+    if ''.join(state) == answer:
+        return count
+    else:
+        return -1
 
 if __name__ == "__main__":
     N = int(input())
-    state = input().strip()
-    print(algorithm(state, 0))
+    state = list(input().strip())
+    answer = input().strip()
+    ans1 = zeroAlgorithm(state[:])
+    ans2 = zeroSwitchAlgorithm(state[:])
+
+    if ans1 > 0 and ans2:
+        print(min(ans1, ans2))
+    elif ans1 > 0 and ans2 == -1:
+        print(ans1)
+    elif ans1 == -1 and ans2 > 0:
+        print(ans2)
+    else:
+        print(-1)
