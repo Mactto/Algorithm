cb118b9673c74d3824e6a47318e9d466aa36894c
mactto <shc9928@gmail.com>
2021-08-27 16:07:42 +0900

    x만큼증가하는n개의숫자

---
+++ programmers/x만큼간격이있는n개의숫자.py
@@ -0,0 +1,10 @@
+def solution(x, n):
+    answer = []
+    for i in range(1, n+1):
+        answer.append(x * i)
+    return answer
+
+
+if __name__ == "__main__":
+    x, n = 2, 5
+    print(solution(x, n))
