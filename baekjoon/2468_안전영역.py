a1dfe3ee78f26636fcfa5e8dfcb1d135003ffe96
Mactto <shc9928@gmail.com>
2021-08-31 11:02:57 +0900

    2468

--- baekjoon/2468_안전영역.py
+++ baekjoon/2468_안전영역.py
@@ -1,29 +1,31 @@
 import sys
-sys.setrecursionlimit(100000)
 input = sys.stdin.readline
 
-def dfs(point, visited):
-    y, x = point
-    visited.append([x,y])
-    print(i, j)
-
-    for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
-        if 0 <= nx < N and 0 <= ny < N:
-            if loc[nx][ny] > 4 and [nx,ny] not in visited:
-                dfs([nx,ny], visited) 
+def dfs(point, visited, k):
+        dx = [-1, 1, 0, 0]
+        dy = [0, 0, -1, 1]
+        x, y = point
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N and loc[nx][ny] > k and visited[nx][ny] == 0:
+                visited[nx][ny] = 1
+                dfs([nx, ny], visited, k)
 
 if __name__ == "__main__":
     N = int(input())
     loc = [list(map(int, input().split())) for _ in range(N)]
-    visited = []
-    result = 0
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    for i in range(N):
-        for j in range(N):
-            if loc[i][j] > 4 and [i,j] not in visited:
-                result += 1
-                dfs([i,j], visited)
-    print(result)
+    answer = 0
+
+    for k in range(max(map(max, loc))):
+        visited = [[0] * N for _ in range(N)]
+        count = 0
+
+        for i in range(N):
+            for j in range(N):
+                if loc[i][j] > k and visited[i][j] != 1:
+                    count += 1
+                    visited[i][j] = 1
+                    dfs([i, j], visited, k)
+        answer = max(count, answer)
+    print(answer)
