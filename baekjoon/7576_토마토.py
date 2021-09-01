434ef9b5aade0b05f26f4c7a668107afe6de6810
mactto <shc9928@gmail.com>
2021-09-01 14:25:14 +0900

    7576

--- baekjoon/7576_토마토.py
+++ baekjoon/7576_토마토.py
@@ -1,33 +1,41 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
-
-
-def bfs():
-    global queue, day
-
-    while queue:
-        day += 1
-        for q in queue:
-            x, y = q
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0 <= nx < N and 0 <= ny < M:
-                    if box[nx][ny] == 0:
-                        box[nx][ny] = 1
-                        queue.append([nx, ny])
-
 
 if __name__ == "__main__":
     M, N = map(int, input().split())
     box = [list(map(int, input().split())) for _ in range(N)]
-    queue = []
-    day = 0
+    queue = deque()
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
+
     for i in range(N):
         for j in range(M):
             if box[i][j] == 1:
-                queue.append([i, j])
+                queue.append([j, i])
+
+    def bfs():
+        while queue:
+            x, y = queue.popleft()
+
+            for k in range(4):
+                nx = x + dx[k]
+                ny = y + dy[k]
+                if 0 <= nx < M and 0 <= ny < N and box[ny][nx] == 0:
+                    queue.append([nx, ny])
+                    box[ny][nx] = box[y][x] + 1
     bfs()
-    print(day)
+    flag = False
+    result = -2
+
+    for i in range(N):
+        for j in range(M):
+            if box[i][j] == 0:
+                flag = True
+            result = max(result, box[i][j])
+    if flag:
+        print(-1)
+    elif result == -1:
+        print(0)
+    else:
+        print(result-1)
