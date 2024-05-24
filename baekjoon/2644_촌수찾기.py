25c975b75fa944f7408e40e5ed59f0f6cba09682
Mactto <shc9928@gmail.com>
2024-05-24 11:28:20 +0900

    2644

--- baekjoon/2644_촌수찾기.py
+++ baekjoon/2644_촌수찾기.py
@@ -1,32 +1,37 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
 
-N = int(input())
-target1, target2 = map(int, input().split())
-M = int(input())
-relations = [list(map(int, input().split())) for _ in range(M)]
+n = int(input())
+p1, p2 = map(int, input().split())
+m = int(input())
 
+relation = [0 for _ in range(n + 1)]
+relation_dict = {}
 
-def bfs(v, count):
+for i in range(1, n+1):
+    relation_dict[i] = []
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    relation_dict[x].append(y)
+    relation_dict[y].append(x)
+
+def bfs(p1, p2):
     queue = deque()
-    queue.append([v, count])
-    visited = [False for _ in range(N+1)]
+    queue.append([p1, 0])
 
     while queue:
-        temp, answer = queue.popleft()
+        target, count = queue.popleft()
 
-        if temp == target2:
-            return answer
+        if target == p2:
+            return relation[p2]
 
-        for v1, v2 in relations:
-            if not visited[temp]:
-                if v1 == temp:
-                    queue.append([v2, answer+1])
-                if v2 == temp:
-                    queue.append([v1, answer+1])
-        visited[temp] = True
+        for r in relation_dict[target]:
+
+            if relation[r] == 0:
+                relation[r] = count + 1
+                queue.append([r, count + 1])
+    
     return -1
+            
 
-
-print(bfs(target1, 0))
+print(bfs(p1, p2))
