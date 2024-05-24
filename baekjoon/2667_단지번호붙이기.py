25bf68e92be1d5ca2c1e9f61e61e54d1ba65f968
Mactto <shc9928@gmail.com>
2024-05-24 11:28:36 +0900

    2667

--- baekjoon/2667_단지번호붙이기.py
+++ baekjoon/2667_단지번호붙이기.py
@@ -1,37 +1,44 @@
-import sys
-input = sys.stdin.readline
+from collections import deque
 
 N = int(input())
-aparts = [list(input().strip()) for _ in range(N)]
-count = 0
-apart_nums = 0
+
+maps = []
+visited = [[False for _ in range(N)] for _ in range(N)]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+for _ in range(N):
+    maps.append(list(map(int, input())))
+
+
+def bfs(x, y):
+    count = 1
+    queue = deque()
+    queue.append([x, y])
+
+    while queue:
+        x, y = queue.popleft()
+
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+
+            if 0 <= nx < N and 0 <= ny < N and maps[nx][ny] and visited[nx][ny] == False:
+                queue.append([nx, ny])
+                visited[nx][ny] = True
+                count += 1
+
+    return count
+
 result = []
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-
-
-def dfs(point):
-    global aparts, apart_nums
-    x, y = point
-    for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
-
-        if 0 <= nx < N and 0 <= ny < N and aparts[ny][nx] == '1':
-            apart_nums += 1
-            aparts[ny][nx] = '2'
-            dfs([nx, ny])
-
 
 for i in range(N):
     for j in range(N):
-        if aparts[i][j] == '1':
-            apart_nums = 0
-            count += 1
-            dfs([j, i])
-            if count > 0 and apart_nums == 0:
-                apart_nums += 1
-            result.append(apart_nums)
-print(count)
+        if maps[i][j] == 1 and visited[i][j] == False:
+            visited[i][j] = True
+            result.append(bfs(i, j))
+
 result.sort()
+print(len(result))
 for r in result:
     print(r)
